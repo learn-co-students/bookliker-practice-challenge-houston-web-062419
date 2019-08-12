1680d1a3c236ead4e744cf333742fdcf99884cfa
Aaron Briscoe <gmail2008@gmail.com>
2019-08-12 15:50:39 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,37 +1,58 @@
-// document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
 
-const booksUrl = 'http://localhost:3000/books'
-const usersUrl = ' http://localhost:3000/users'
-const 
-
-fetch(booksUrl)
-.then(res => res.json())
-.then(books => {
-    books.forEach(book => renderBook(book))
-
-})
-
-function renderBook(book){
-    const div = document.createElement('div')
-    div.className = 'book'
-
-    const h2 = document.createElement('h2')
-    h2.className = 'click-title'
-
+    const booksUrl = 'http://localhost:3000/books'
+    const ul = document.getElementById('list')
+    const div = document.querySelector('#show-panel')
+    const booksDiv = document.querySelector('#list-panel')
     
+    fetch(booksUrl)
+    .then(res => res.json())
+    .then(books => {
+        books.forEach(book => renderBook(book))
+        
+    })
+
+    function renderBook(book){
 
 
-}
+        const li = document.createElement('li')
+        // li.setAttribute('book-id', book.id)
+        li.innerText = book.title
+        
+        li.addEventListener('click', () => {
+          showBook(book)
+        })
+
+        ul.append(li)
+        booksDiv.append(ul)
+
+    }
+
+    function showBook(book){
+        div.innerHTML = ""
+        const h2 = document.createElement('h2')
+        h2.className = 'click-title'
+        h2.innerText = book.title
+
+        const image = document.createElement('img')
+        image.src = book.img_url
+        image.className = 'book-cover'
+
+        const p = document.createElement('p')
+        p.innerText = book.description
+
+        const btn = document.createElement('button')
+        btn.className = 'like-btn'
+        btn.setAttribute('book-id', book.id)
+        btn.innerText = 'Read Book'
+        btn.addEventListener('click', {
+
+        })
+
+        div.append(h2, image, p, btn) 
+    }
 
 
-/* <div class="book">
-        <h2 class='click-title'>title</h2>
-        Once clicked on, needs to go to a page with this on it?
-        <img src=book_img_url class="book-cover" />
-        <p>description</p>
-        <p class='liked-by'> Liked By: </p>
-       // id = l'ist' <li>Users.username</li>
-        <button class="like-btn">Like <3</button>
-    </div> */
 
 
+});
